8b83d21db7a82c6afcff75035e7b04d8c7417f67
niteshn1 <117146760+niteshn1@users.noreply.github.com>
2023-10-13 12:47:43 +0530

    changes

--- src/app/components/news.jsx
+++ src/app/components/news.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-function News() {
+const News = () => 
   let [value, setValue] = useState("nitesh");
   async function ak() {
     let response = await fetch(
